5f6b9dcd37b8804aec9cbf6e35290250c9922819
thaborak <thaborak@gmail.com>
2018-03-20 15:28:42 -0400

    path for Note

--- routes/notes.js
+++ routes/notes.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const { MONGODB_URI } = require('../config');
 
-const Note = require('../models/note');
+const Note = require('../models/notes');
 
 
 /* ========== GET/READ ALL ITEM ========== */

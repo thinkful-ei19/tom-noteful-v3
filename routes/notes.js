d44b31330191b4c5f2f1402b2ef721d853dd719c
thaborak <thaborak@gmail.com>
2018-03-20 16:14:07 -0400

    route put notes added

--- routes/notes.js
+++ routes/notes.js
@@ -42,7 +42,7 @@ router.get('/notes', (req, res, next) => {
       console.error(err);
     });
 });
-  
+
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
   const noteId = req.params.id;
@@ -57,9 +57,9 @@ router.get('/notes/:id', (req, res, next) => {
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/notes', (req, res, next) => {
-  const { title, content, folder_id, tags } = req.body; 
+  const { title, content, folder_id, tags } = req.body;
   const newItem = { title, content, folder_id };
-  
+
   /***** Never trust users - validate input *****/
   if (!newItem.title) {
     const err = new Error('Missing `title` in request body');
@@ -72,19 +72,37 @@ router.post('/notes', (req, res, next) => {
     content,
     folder_id,
     tags,
-  })  
+  })
     .then(results => {
-      res.location(`${req.originalUrl}`).status(201).json({results})
+      res.location(`${req.originalUrl}`).status(201).json({ results });
     });
-  
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
+  const noteId = req.params.id;
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateableFields = ['title', 'content'];
 
-  console.log('Update a Note');
-  res.json({ id: 2 });
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  /***** Never trust users - validate input *****/
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  mongoose.connect(MONGODB_URI);
+  return Note.findByIdAndUpdate(noteId, updateObj)
+    .then(results => {
+      res.location(`${req.originalUrl}`).status(200).json({ results });
+    });
 
 });
 
